47f2f1acbf95b2406e85f73e31a9ac7c4deaf0c6
Anthony Viera <anthonyviera4@gmail.com>
2022-08-07 19:05:15 -0500

    Home skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import styles from "./app.module.css";
 import Navbar from "./navbar/Navbar";
+import Home from "./home/Home";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Home />
     </div>
   );
 }
