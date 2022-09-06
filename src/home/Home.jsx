a05aaacefcd18c269a159265a39932936c43a3c6
Anthony Viera <anthonyviera4@gmail.com>
2022-09-06 06:14:36 -0500

    Use react icons and use down right arrow icon

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -3,21 +3,18 @@ import styles from "./home.module.css";
 import Typed from "react-typed";
 import me from "../assets/me.jpg";
 import { Button } from "react-bootstrap";
-import Arrow from "../assets/arrow-right.svg";
+import { FiCornerRightDown } from "react-icons/fi";
 
 export default function Home() {
   let sen1 = "Hello, my name is Anthony";
   let sen2 = "I am a full stack developer";
-  let sen3 = "I like working on the frontend";
-  let sen4 = "I like working on the backend";
-  let sen5 = "I like using the cloud";
 
   return (
     <div className={styles.Home}>
       <img className={styles.Me} src={me} alt="this me" />
       <h3>
         <Typed
-          strings={[sen1, sen2, sen3, sen4, sen5]}
+          strings={[sen1, sen2]}
           typeSpeed={60}
           backSpeed={40}
           loop
@@ -25,7 +22,7 @@ export default function Home() {
       </h3>
       <Button className={styles.Intro} variant="outline-primary">
         Intro
-        <img src={Arrow} alt="arrow-right" />
+        <FiCornerRightDown size={"2em"} className={styles.Arrow} />
       </Button>
     </div>
   );
