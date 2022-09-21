dd1aa00b7e3bf8e35512c4293f3f67c4146ed913
Anthony Viera <anthonyviera4@gmail.com>
2022-09-20 20:51:34 -0500

    Boxes boxes boxes, nuff said

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,7 +5,24 @@ export default function Projects() {
   return (
     <div id="Projects" className={styles.Projects}>
       <h3>Projects</h3>
-      <div></div>
+      <div className={styles.List}>
+        <div className={styles.Project}>
+
+        </div>
+        <div className={styles.Project}>
+
+        </div>
+        <div className={styles.Project}>
+
+        </div>
+        <div className={styles.Project}>
+
+        </div>
+        <div className={styles.Project}>
+
+        </div>
+      </div>
+
     </div>
   );
 }
